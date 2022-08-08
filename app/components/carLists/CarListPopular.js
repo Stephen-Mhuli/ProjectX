87da12a343145035f53437bc26d134fedda3824b
Stephen-Mhuli <stevemshible@gmail.com>
2022-08-08 10:23:55 +0300

    added a ListHeaderComponent prop in FlatList to make content on top scrollable

--- app/components/carLists/CarListPopular.js
+++ app/components/carLists/CarListPopular.js
@@ -2,16 +2,25 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import GlobalStyles from "../../GlobalStyles";
 import CarListPopularItem from "./CarListPopularItem";
+import CarListHotDeal from "./CarListHotDeal";
+import SearchBarComponent from "../searchBar";
+import Header from "../Header";
 
 const CarListPopular = ({ navigation, cars }) => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={GlobalStyles.commonHeading}>Popular</Text>
-      </View>
       <FlatList
+        ListHeaderComponent={
+          <>
+            <Header navigation={navigation} />
+            <SearchBarComponent navigation={navigation} />
+            <CarListHotDeal navigation={navigation} cars={cars} />
+            <View>
+              <Text style={GlobalStyles.commonHeading}>Popular</Text>
+            </View>
+          </>
+        }
         data={cars}
-        nestedScrollEnabled={true}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <CarListPopularItem navigation={navigation} item={item} />
--- app/screens/HomeScreen/index.js
+++ app/screens/HomeScreen/index.js
@@ -1,22 +1,15 @@
 //libraries
-import { StyleSheet, Dimensions, SafeAreaView } from "react-native";
-import SearchBarComponent from "../../components/searchBar";
+import { StyleSheet, Dimensions, SafeAreaView, ScrollView } from "react-native";
 import { StatusBar } from "react-native";
 
 // Components
-import CarListHotDealCard from "../../components/carLists/CarListHotDeal";
 import CarListPopular from "../../components/carLists/CarListPopular";
-import Header from "../../components/Header";
-import cars from "../../../Common/Data/cars"
+import cars from "../../../Common/Data/cars";
 let { width } = Dimensions.get("window");
 
 export default function HomeScreen({ navigation }) {
-
   return (
     <SafeAreaView style={styles.container}>
-      <Header navigation={navigation} />
-      <SearchBarComponent navigation={navigation} />
-      <CarListHotDealCard navigation={navigation} cars={cars} />
       <CarListPopular navigation={navigation} cars={cars} />
     </SafeAreaView>
   );
